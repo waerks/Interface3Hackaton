a77e37355f8f121e7deaab401c1ec961819890d6
Marie-Laure <143529477+MaryMaryOhsoFunny@users.noreply.github.com>
2024-10-24 16:25:09 +0200

    added sounds

--- js/game.js
+++ js/game.js
@@ -31,6 +31,7 @@ let wrongAnswerSound;
 let playButtonSound;
 let nextButtonSound;
 let restartButtonSound;
+let growingPlantSound;
 
 function preload() {
   this.load.image("background", "./assets/Sprites/background.png");
@@ -40,15 +41,19 @@ function preload() {
   this.load.image("star", "./assets/Sprites/Star.png");
   this.load.image("restart", "./assets/Sprites/Restart.png");
   this.load.image("logo", "./assets/Sprites/Quiz1.png");
+  this.load.image("soundOn", "./assets/Sprites/soundOn.png");
+  this.load.image("soundOff", "./assets/Sprites/soundOff.png");
 
   // this.load.image("muteButton", "./assets/Sprites/Buttons-note.png");
   // Sons
-  this.load.audio("backgroundMusic", "./assets/Sound/Spring Break.wav");
+  this.load.audio("backgroundMusic", "./assets/Sound/Galactic Odyssey.wav");
   this.load.audio("goodAnswerSound", "./assets/Sound/goodAnswer.wav");
   this.load.audio("wrongAnswerSound", "./assets/Sound/badAnswer.wav");
   this.load.audio("playButtonSound", "./assets/Sound/playButton.wav");
-  this.load.audio("nextButtonSound", "./assets/Sound/nextButton.wav");
-  this.load.audio("restartButtonSound", "./assets/Sound/restartButton.wav");
+  this.load.audio("nextButtonSound", "./assets/Sound/selectButtons.wav");
+  this.load.audio("restartButtonSound", "./assets/Sound/startButton.wav");
+  this.load.audio("growingPlantSound", "./assets/Sound/growingPlant.wav");
+  // this.load.audio("selectButtons", "./assets/Sound/selectButtons.wav");
 
   this.load.json("questions", "./assets/Data/Questions.json");
 }
@@ -57,21 +62,31 @@ function create() {
   // construire l'objet questions à partir du JSON
   questions = this.cache.json.get("questions").questions;
 
-  // préparer les sons :
-  goodAnswerSound = this.sound.add("goodsound");
-  wrongAnswerSound = this.sound.add("wrongsound");
-  // préparer les sons :
-  goodAnswerSound = this.sound.add("goodsound");
-  wrongAnswerSound = this.sound.add("wrongsound");
+  // dessiner l'image de fond
+  let backImage = this.add.image(0, 0, "background");
+  backImage.setOrigin(0, 0);
+  backImage.setScale(0.5);
 
-  // dessiner l'image de fond
-  let backImage = this.add.image(0, 0, "background");
-  backImage.setOrigin(0, 0);
-  backImage.setScale(0.5);
-  // dessiner l'image de fond
-  let backImage = this.add.image(0, 0, "background");
-  backImage.setOrigin(0, 0);
-  backImage.setScale(0.5);
+  const music = this.sound.add("backgroundMusic", {
+    loop: true,
+    volume: 0.5
+  });
+
+  music.play();
+
+
+
+  const button = this.add.image(50, 50, "soundOn").setInteractive();
+
+  button.on('pointerdown', function() {
+    if (music.mute) {
+      music.setMute(false);
+      button.setTexture("soundOn");
+    } else {
+      music.setMute(true);
+      button.setTexture("soundOff");
+    }
+  });
 
   // dessiner l'image pour les questions
   let questionPanelImage = this.add.image(
@@ -110,55 +125,23 @@ function create() {
     });
     answerText[i].setVisible(false);
   }
-  // dessiner l'image pour les questions
-  let questionPanelImage = this.add.image(
-    config.width / 2,
-    100,
-    "questionpanel"
-  );
-  questionPanelImage.setScale(0.5);
+  
 
-  // dessiner les 3 images pour les 3 réponses
-  for (let i = 0; i < 3; i++) {
-    answerPanelImage[i] = this.add.image(
-      config.width / 2,
-      220 + 100 * i,
-      "answerpanel"
-    );
-    answerPanelImage[i].setScale(1.2, 0.8);
-    answerPanelImage[i].on("pointerdown", () => {
-      checkAnswer(i);
-    });
-  }
+  // dessiner le logo de départ
+  logoImage = this.add.image(config.width / 2, 100, "logo");
+  logoImage.setScale(0.8);
 
-  // écrire la question
-  questionText = this.add.text(150, 80, "", {
-    fontFamily: "Arial",
-    fontSize: 18,
-    color: "#00ff00",
+  // dessiner le bouton pour question suivante
+  nextQuestionImage = this.add.image(config.width / 2, 510, "nextquestion").setInteractive();
+  nextQuestionImage.on("pointerdown", nextQuestion);
+  nextQuestionImage.setScale(0.4);
+  //nextQuestionImage.setVisible(false);
+  nextButtonSound = this.sound.add("nextButtonSound");
+  nextQuestionImage.on("pointerdown", function() {
+    nextButtonSound.play();
   });
-
-  // écrire les 3 réponses
-  for (let i = 0; i < 3; i++) {
-    answerText[i] = this.add.text(150, 200 + i * 100, "", {
-      fontFamily: "Arial",
-      fontSize: 18,
-      color: "#ffff00",
-    });
-    answerText[i].setVisible(false);
-  }
-
-  // dessiner le logo de départ
-  logoImage = this.add.image(config.width / 2, 100, "logo");
-  logoImage.setScale(0.8);
-
-  // dessiner le bouton pour question suivante
-  nextQuestionImage = this.add
-    .image(config.width / 2, 510, "nextquestion")
-    .setInteractive();
-  nextQuestionImage.on("pointerdown", nextQuestion);
-  nextQuestionImage.setScale(0.4);
-  //nextQuestionImage.setVisible(false);
+  
+  
 
   // afficher 10 étoiles en bas de l'écran et on les rend invisible
   for (let i = 0; i < 10; i++) {
@@ -166,58 +149,18 @@ function create() {
     starImage[i].setScale(0.25);
     starImage[i].setVisible(false);
   }
-  // dessiner le logo de départ
-  logoImage = this.add.image(config.width / 2, 100, "logo");
-  logoImage.setScale(0.8);
-
-  // dessiner le bouton pour question suivante
-  nextQuestionImage = this.add
-    .image(config.width / 2, 510, "nextquestion")
-    .setInteractive();
-  nextQuestionImage.on("pointerdown", nextQuestion);
-  nextQuestionImage.setScale(0.4);
-  //nextQuestionImage.setVisible(false);
-
-  // afficher 10 étoiles en bas de l'écran et on les rend invisible
-  for (let i = 0; i < 10; i++) {
-    starImage[i] = this.add.image(30 + i * 60, 600, "star");
-    starImage[i].setScale(0.25);
-    starImage[i].setVisible(false);
-  }
+ 
 
   // Ajouter le bouton Restart
   restartImage = this.add.image(300, 350, "restart").setInteractive();
   restartImage.on("pointerdown", restart);
   restartImage.setVisible(false);
-  // Ajouter le bouton Restart
-  restartImage = this.add.image(300, 350, "restart").setInteractive();
-  restartImage.on("pointerdown", restart);
-  restartImage.setVisible(false);
+  restartButtonSound = this.sound.add("restartButtonSound");
+ 
+  
+  
+  
 
-  // créer une animation sur un panel
-  for (let i = 0; i < 3; i++) {
-    tweenGoodAnswerPanel[i] = this.tweens.add({
-      targets: answerPanelImage[i],
-      scaleY: 1.2,
-      scaleX: 1.5,
-      duration: 200,
-      ease: "Power2",
-      yoyo: true,
-      loop: 0,
-      paused: true,
-    });
-  }
-  for (let i = 0; i < 3; i++) {
-    tweenWrongAnswerPanel[i] = this.tweens.add({
-      targets: answerPanelImage[i],
-      angle: 15,
-      duration: 40,
-      ease: "Power2",
-      yoyo: true,
-      loop: 2,
-      paused: true,
-    });
-  }
   // créer une animation sur un panel
   for (let i = 0; i < 3; i++) {
     tweenGoodAnswerPanel[i] = this.tweens.add({
@@ -256,29 +199,16 @@ function checkAnswer(answerNumber) {
   answerPanelImage[questions[questionIndex].goodAnswer].tint = 0x22ff22;
   if (answerNumber == questions[questionIndex].goodAnswer) {
     score += 1;
-    goodAnswerSound.play();
-    tweenGoodAnswerPanel[answerNumber].play();
+    // growingPlantSound = this.sound.add("growingPlantSound");
+    // growingPlantSound.play();
+    
+    // selectButtons.play();
+    // tweenGoodAnswerPanel[answerNumber].play();
   } else {
     answerPanelImage[answerNumber].tint = 0xff0000;
     starImage[questionIndex].alpha = 0.4;
-    wrongAnswerSound.play();
-    tweenWrongAnswerPanel[answerNumber].play();
-  }
-  for (let i = 0; i < 3; i++) {
-    answerPanelImage[i].disableInteractive();
-  }
-  nextQuestionImage.setVisible(true);
-  starImage[questionIndex].setVisible(true);
-  answerPanelImage[questions[questionIndex].goodAnswer].tint = 0x22ff22;
-  if (answerNumber == questions[questionIndex].goodAnswer) {
-    score += 1;
-    goodAnswerSound.play();
-    tweenGoodAnswerPanel[answerNumber].play();
-  } else {
-    answerPanelImage[answerNumber].tint = 0xff0000;
-    starImage[questionIndex].alpha = 0.4;
-    wrongAnswerSound.play();
-    tweenWrongAnswerPanel[answerNumber].play();
+    // selectButtons.play();
+    // tweenWrongAnswerPanel[answerNumber].play();
   }
 }
 
@@ -304,6 +234,8 @@ function nextQuestion() {
     restartImage.setVisible(true);
   }
   nextQuestionImage.setVisible(false);
+
+  nextButtonSound.play();
 }
 
 function restart() {
@@ -329,4 +261,6 @@ function restart() {
     answerText[i].setVisible(true);
   }
   questionText.text = questions[questionIndex].title;
+  restartButtonSound.play();
+
 }
