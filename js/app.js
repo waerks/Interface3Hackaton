b84c0bc7d0a8e97d07b22adc81414d5d294ad0fb
Anaïs <a.aerts@interface3.be>
2024-10-25 09:52:34 +0200

    add manifest

--- js/app.js
+++ js/app.js
@@ -1,31 +1,33 @@
 // SERVICE WORKER
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
-        .then(registration => {
-          console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, err => {
-          console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-  }
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker enregistré avec succès avec le scope:', registration.scope);
+      })
+      .catch(err => {
+        console.log('Échec de l\'enregistrement du ServiceWorker:', err);
+      });
+  });
+}
 
-//   INSTALL PWA
+// INSTALL PWA
 let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
-  // Empêche le navigateur d'afficher le prompt automatiquement
+  // Empêche le navigateur d'afficher le prompt d'installation automatique
   e.preventDefault();
   deferredPrompt = e;
-  
-  // Affiche ton bouton d'installation
-  document.querySelector('#install-button').style.display = 'block';
 
-  document.querySelector('#install-button').addEventListener('click', (e) => {
+  // Affiche le bouton d'installation
+  const installButton = document.querySelector('#install-button');
+  installButton.style.display = 'block';
+
+  installButton.addEventListener('click', (e) => {
     // Affiche le prompt d'installation
     deferredPrompt.prompt();
     
-    // Attend la réponse de l'utilisateur
+    // Attends la réponse de l'utilisateur
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
         console.log('L\'utilisateur a accepté l\'installation');
@@ -35,4 +37,10 @@ window.addEventListener('beforeinstallprompt', (e) => {
       deferredPrompt = null; // Réinitialise le prompt
     });
   });
+});
+
+// Masque le bouton une fois l'installation terminée
+window.addEventListener('appinstalled', (evt) => {
+  console.log('L\'application a été installée');
+  document.querySelector('#install-button').style.display = 'none';
 });
